709238a154c5868a258feb77ae1bb4e457b26657
jfremond <jfremond@student.42.fr>
2022-01-14 21:45:28 +0100

    Optimization

--- sources/sort_three.c
+++ sources/sort_three.c
@@ -6,7 +6,7 @@
 /*   By: jfremond <jfremond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 13:59:19 by jfremond          #+#    #+#             */
-/*   Updated: 2021/09/02 20:54:02 by jfremond         ###   ########.fr       */
+/*   Updated: 2022/01/14 21:43:06 by jfremond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	sort_three(t_list **list, t_infos *infos)
 	get_infos(*tmp, infos);
 	while (infos->sorted == 0)
 	{
-		if (infos->s_pos == 1 && infos->b_pos == 2)
+		if (infos->b_pos == 2)
 			write_sa(tmp);
 		else if (infos->b_pos == 0)
 			write_ra(tmp);
@@ -38,7 +38,7 @@ void	reverse_sort_three(t_list **list, t_infos *infos)
 	get_infos(*tmp, infos);
 	while (infos->rsorted == 0)
 	{
-		if (infos->s_pos == 2 && infos->b_pos == 1)
+		if (infos->s_pos == 2)
 			write_sb(tmp);
 		else if (infos->s_pos == 0)
 			write_rb(tmp);
